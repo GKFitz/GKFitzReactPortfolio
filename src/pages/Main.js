c4d13a60f5878b3aff0c3bedd1858f63ce21f9fc
Gillian Fitzgerald <gillianfitzgerald@gillians-mbp.home>
2021-04-13 16:03:47 -0400

    added the css stlying from portfolio 2, and fixed the image file paths

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import SearchBar from '../components/NavBar.js';
-import SearchBar from '../components/MyWork.js';
+import NavBar from '../components/NavBar.js';
+import MyWork from '../components/MyWork.js';
 
 export default class Main extends Component {
     render() {
